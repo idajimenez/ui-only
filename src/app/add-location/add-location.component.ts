fdbefb086d2b27cf24970438f8880ab4f50be3e0
Ida Jimenez <ida@old.st>
2021-11-12 12:28:11 +0800

    add location done

--- src/app/add-location/add-location.component.ts
+++ src/app/add-location/add-location.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class AddLocationComponent implements OnInit {
 
     // TODO fetch actual data
-    public cityOptions: any = dummyCity;
-    public facilityOptions: any = [];
-    public floorOptions: any = [];
+    public cityData: any = dummyCity;
+    public facilityData: any = [];
+    public floorData: any = [];
 
-    public selectCitynm: string = '';
-    public selectFacilitynm: string = '';
-    public selectFloornm: string = '';
+    public selectedCity: string = '';
+    public selectedFacility: string = '';
+    public selectedFloor: string = '';
 
     public errorSection?: 'city' | 'facility' | 'floor';
 
@@ -24,46 +24,81 @@ export class AddLocationComponent implements OnInit {
     }
 
     public handleAdd = (value: string, key: string) => {
-        console.log(value, key);
         if (key === 'city') {
-            const cityIndex = this.cityOptions.findIndex((city: any) => city.citynm === value);
-            console.log(cityIndex);
+            const cityIndex = this.cityData.findIndex((city: any) => city.citynm === value);
 
             if (cityIndex === -1) {
-                this.cityOptions.push({ citynm: value, facilities: [] });
+                this.cityData.push({ citynm: value, facilities: [] });
             } else {
                 this.errorSection = key;
             }
         } else if (key === 'facility') {
-            const facilityIndex = this.facilityOptions.findIndex((item: any) => item.facility === value);
+            const facilityIndex = this.facilityData.findIndex((item: any) => item.facility === value);
 
             if (facilityIndex === -1) {
-                this.facilityOptions.push({ facilityid: this.facilityOptions, facility: value });
+                this.facilityData.push({ facilityid: this.facilityData, facility: value });
             } else {
                 this.errorSection = key;
             }
         } else if (key === 'floor') {
-            const floorIndex = this.floorOptions.findIndex((floor: number) => floor === parseInt(value));
+            const floorIndex = this.floorData.findIndex((floor: number) => floor === parseInt(value));
 
             if (floorIndex === -1) {
-                this.floorOptions.push(parseInt(value));
+                this.floorData.push(parseInt(value));
             } else {
                 this.errorSection = key;
             }
         }
     }
+
+    getOptions(key: string) {
+        const arr = key === 'city' ? this.cityData : key === 'facility' ? this.facilityData : this.floorData;
+
+        return arr.map((item: any) => {
+            if (key === 'city') {
+                return item.citynm;
+            } else if (key === 'facility') {
+                return item.facility;
+            } else {
+                return item;
+            }
+        })
+    }
+
+    public setSelectedLocation = (value: string, key: string) => {
+        console.log(value, key)
+        // TODO Call api to retrieve list of options
+        if (key === 'city') {
+            this.selectedCity = value;
+            this.facilityData = dummyFacility.filter((item: any) => item.citynm === value);
+            this.floorData = [];
+            this.selectedFacility = '';
+            this.selectedFloor = ''
+        } else if (key === 'facility') {
+            this.selectedFacility = value;
+
+            const facility = this.facilityData.find((item: any) => item.facility === value);
+            const floorIndex = dummyFloor.findIndex((item: any) => item.facilityid === facility.facilityid);
+
+            this.floorData = dummyFloor[floorIndex].floors;
+            this.selectedFloor = ''
+        } else {
+            this.selectedFloor = value;
+        }
+    }
 }
 
 const dummyFloor = [
-    { "facilityid": 1, "floors": [1, 2, 3, 4] },
-    { "facilityid": 2, "floors": [1, 2, 3, 4, 6] },
-    { "facilityid": 3, "floors": [1, 2] }
+    { "facilityid": 1, "floors": ['1', '2', '3', '4'] },
+    { "facilityid": 2, "floors": ['1', '2', '3', '4', '6'] },
+    { "facilityid": 3, "floors": ['1', '2'] }
 ]
 
 const dummyFacility = [
-    { "facilityid": 1, "facility": "officia proident" },
-    { "facilityid": 2, "facility": "velit laborum" },
-    { "facilityid": 3, "facility": "nostrud culpa" }
+    { "facilityid": 1, "facility": "officia proident", "citynm": "officia magna" },
+    { "facilityid": 2, "facility": "velit laborum", "citynm": "enim velit" },
+    { "facilityid": 4, "facility": "velit 4534535", "citynm": "enim velit" },
+    { "facilityid": 3, "facility": "nostrud culpa", "citynm": "ex excepteur" }
 ]
 
 const dummyCity = [
--- src/app/select-list/select-list.component.ts
+++ src/app/select-list/select-list.component.ts
@@ -1,27 +1,50 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'select-list',
   templateUrl: './select-list.component.html',
   styleUrls: ['./select-list.component.scss']
 })
-export class SelectListComponent implements OnInit {
+export class SelectListComponent implements OnInit, OnChanges {
   @Input() type: string = '';
   @Input() options: any[] = [];
   @Input() handleAdd: ((value: string, type: string) => void) | undefined;
+  @Input() isDisabled: boolean = true;
+  @Input() parent?: string = '';
+  @Input() setSelectedLocation: ((value: string, type: string) => void) | undefined;
+  @Input() selected?: string = '';
 
   public newEntry: string = '';
+  public isExisting: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  handleChange(e: any) {
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.disabled) {
+      this.newEntry = ' ';
+      this.isExisting = false;
+    }
+  }
+
+  public handleChange = (e: any) => {
+    this.isExisting = false;
     this.newEntry = e.target.value
   }
 
   sendNewEntry() {
-    this.handleAdd!(this.newEntry, this.type);
+    console.log(this.options);
+    if (this.options.indexOf(this.newEntry) === -1) {
+      this.handleAdd!(this.newEntry, this.type);
+      this.newEntry = '';
+    } else {
+      this.isExisting = true;
+    }
+  }
+
+  handleSelectItem(value: string) {
+    this.setSelectedLocation!(value, this.type);
   }
 }
